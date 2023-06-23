9398d3138bb6195bbd9ff718310e1b46bea689ce
Rodrigo Rocha Souza <rodriguinhorsouza@gmail.com>
2023-06-23 17:19:12 -0300

    cronometro v1

---
+++ Projetos Pessoais/React/exercicios/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/navbar';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="h1">Calculadora</h1>
+    <nav className="nav">
+      <Navbar />
+    </nav>
+    </div>
+  );
+}
+
+
---
+++ Projetos Pessoais/React/exercicios/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Nav () {
+    return (
+        <div>
+            <nav>
+                <h1>Teste</h1>
+            </nav>
+        </div>
+
+    );
+}
